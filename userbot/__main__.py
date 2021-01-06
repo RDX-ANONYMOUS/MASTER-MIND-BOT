58d50145006e5cea2f15b674dfcd1aee325623f9
Seoul Morimoto <76898209+RDX-ANONYMOUS@users.noreply.github.com>
2021-01-06 22:32:35 +0530

    LULZ

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -49,7 +49,7 @@ for name in files:
 
 import userbot._core
 
-print("ThunderUserbot has started working.Enjoy")
+print("MASTERMIND USERBOT IS ON AB BC JAAKE TYPE KARO  .alive, OR .ping ")
 
 if len(argv) not in (1, 3, 4):
     bot.disconnect()
